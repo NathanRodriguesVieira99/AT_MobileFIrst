a3dd1a1fac7837c3d989915a99baa51bcf4e9ffc
NathanRodrigues59 <gtafive36910@gmail.com>
2024-09-30 10:39:42 +0000

    ..

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -1,47 +1,44 @@
-import React from 'react'
+import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 
-import './Contato.scss'
+import "./Contato.scss";
 
 function Contato() {
   return (
     <>
-
-      <h1 style={{ color: 'red' }}>Redes do candidato</h1>
-      <div id='contato' className='contatos'>
+      <h1 style={{ color: "red" }}>Redes do candidato</h1>
+      <div id="contato" className="contatos">
         <ul>
-          <a href="" target='_blank'>
-            <li className='wpp'>
+          <a href="#" target="_blank">
+            <li className="wpp">
               <FaWhatsapp />
             </li>
           </a>
-          <a href="" target='_blank'>
-            <li className='email'>
+          <a href="#" target="_blank">
+            <li className="email">
               <MdOutlineEmail />
             </li>
           </a>
-          <a href="" target='_blank'>
-            <li className='ig'>
+          <a href="https://www.instagram.com/" target="_blank">
+            <li className="ig">
               <FaInstagram />
-
             </li>
           </a>
-          <a href="" target='_blank'>
-
-            <li className='ttk'>
+          <a
+            href="https://www.tiktok.com/login?lang=pt-BR&redirect_url=https%3A%2F%2Fwww.tiktok.com%2Fupload%3Flang%3Dpt-BR"
+            target="_blank"
+          >
+            <li className="ttk">
               <FaTiktok />
-
             </li>
           </a>
         </ul>
-
-
       </div>
     </>
-  )
+  );
 }
 
-export default Contato
+export default Contato;
